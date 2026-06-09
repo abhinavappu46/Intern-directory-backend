8aa4e0cb5173310ff74f2a4d4c074e50b928892a
abhinavappu46 <abhinav@gmail.com>
2026-06-09 16:44:54 +0530

    7 commit

--- Server.js
+++ Server.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:"https://intern-directory.onrender.com"
+}));
 app.use("/api", userRoute);
 app.use("/api", FetchRoute);
 app.use("/api", DeleteRoute);
